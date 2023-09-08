f71a5c2a6a2ded7f852f2369ee8848d9c3e28bbc
Ezerike Caleb Chikere <cezerike@gmail.com>
2023-09-08 12:33:40 +0100

    new backend setup

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,6 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import categoryItems from './data/categoryItems.js'
-import newArrivalsRouter from './routes/newArrivalsRoute.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
@@ -21,21 +19,15 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('API is running....')
 })
-app.get('/api/categoryItems', (req, res) => {
-  res.json(categoryItems)
-})
-
 app.use('/api/product', productRouter)
-
-app.use('/api/categoryItems', newArrivalsRouter)
 
 app.use('/api/users', userRoutes)
 
 app.use('/api/orders', orderRoutes)
 
-// app.use(notFound)
+app.use(notFound)
 
-// app.use(errorHandler)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
